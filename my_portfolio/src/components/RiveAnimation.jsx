6b1840671574ab6047de306245021c5b1114d36c
laithmh <ponishermh97@gmail.com>
2025-11-07 10:11:13 +0300

    rive fix path

--- my_portfolio/src/components/RiveAnimation.jsx
+++ my_portfolio/src/components/RiveAnimation.jsx
@@ -1,12 +1,12 @@
 
-import React from 'react'
+
 import { useRive, useStateMachineInput } from '@rive-app/react-canvas'
 
 const RiveAnimation = () => {
   const STATE_MACHINE_NAME = 'State Machine 1' // rename to match your Rive file
   
   const { rive, RiveComponent } = useRive({
-    src: '/rive/6273-12187-a-moon.riv',
+    src: 'rive/6273-12187-a-moon.riv',
     stateMachines: STATE_MACHINE_NAME,
     autoplay: true,
   })
